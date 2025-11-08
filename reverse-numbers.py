1bb5cf8f57e93f78ce0e133da19d444f74bc1a01
Gisela <gisela.pardini15@gmail.com>
2025-11-08 02:22:19 -0300

    reverse-numbers

---
+++ reverse-numbers.py
@@ -0,0 +1,10 @@
+def reverseNumbers(ar):
+  lastPosition = len(ar)
+  reversedNumbers = []
+
+  for i in  range(len(ar)-1,-1,-1):
+    reversedNumbers.append(ar[i])
+  return reversedNumbers
+
+reversedNumbers = reverseNumbers([1,2,3])
+print(reversed)  # Output: [3, 2, 1]
